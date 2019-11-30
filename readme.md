237514bacae34f8f7e19da0b7551ef90ed1c5a2e
NikolayShishkin <r58korin@gmail.com>
2019-11-30 14:36:38 +0300

    feat: change webpack setting

--- readme.md
+++
@@ -1,3 +0,0 @@
-Install dependencies with 'npm install'
-Run project with 'npm run start'
-Build project with 'npm run build'
---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
